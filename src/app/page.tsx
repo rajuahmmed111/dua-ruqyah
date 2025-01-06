860415a0c516a07eb10a1ea9e3b888896aa237ba
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-06 21:50:19 +0600

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import { Header } from "@/components/header";
 import { DuaContent } from "@/components/dua-content";
 import { CategoriesSidebar } from "@/components/sidebar";
 import { SettingsPanel } from "@/components/settings-sidebar";
+import { VerticalNav } from "@/components/VerticalNav";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex">
+      <div>
+        <VerticalNav />
+      </div>
+
       <div className="flex flex-col w-full">
         <Header />
         <div className="flex flex-1">
